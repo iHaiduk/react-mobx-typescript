341c2daa6bd0274a2d8246b11d55bae1c23cb76a
Ihor Haiduk <cookkycok@gmail.com>
2017-10-25 11:41:38 +0300

    small fix browserHistory

--- store/routing/index.ts
+++ store/routing/index.ts
@@ -2,10 +2,10 @@ import { RouterStore, syncHistoryWithStore } from "mobx-react-router";
 
 let browserHistory;
 if (process.env.BROWSER) {
-    const createBrowserHistory = require("history/createBrowserHistory");
+    const createBrowserHistory = require("history/createBrowserHistory").default;
     browserHistory = createBrowserHistory();
 } else {
-    const createMemoryHistory = require("history/createMemoryHistory");
+    const createMemoryHistory = require("history/createMemoryHistory").default;
     browserHistory = createMemoryHistory();
 }
 
